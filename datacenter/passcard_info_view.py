905702279a731f5348a77caefc6cdc4d5c106cd4
evgeny1337 <genya-sozykin@mail.ru>
2024-10-31 22:28:36 +0500

    Add README and fix code

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -8,13 +8,13 @@ def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     this_passcard_visits = [
         {
-            'entered_at': x.entered_at,
-            'duration': x.format_duration(),
-            'is_strange': x.is_visit_long()
+            'entered_at': visit.entered_at,
+            'duration': visit.format_duration(),
+            'is_strange': visit.is_visit_long()
         }
-        for x in Visit.objects.filter(passcard=passcard)
+        for visit in Visit.objects.filter(passcard=passcard)
     ]
-    # Программируем здесь
+
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,3 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
@@ -6,11 +5,11 @@ from django.shortcuts import render
 def storage_information_view(request):
     non_closed_visits = [
         {
-            'who_entered': x.passcard.owner_name,
-            'entered_at': x.entered_at,
-            'duration': x.format_duration()
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.entered_at,
+            'duration': visit.format_duration()
         }
-        for x in Visit.objects.all() if x.leaved_at == None
+        for visit in Visit.objects.filter(leaved_at__isnull=True)
     ]
     context = {
         'non_closed_visits': non_closed_visits,
